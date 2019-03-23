e7db865edd8fddf43c81af0fcc023c7524320da9
yamamoto yuya <chuo.yamamoto0911@gmail.com>
2019-03-23 10:28:09 +0000

    Finish static page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # ページごとにタイトルを返す。
+  def full_title(page_title = "")
+    base_title = "勤怠管理システム"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
