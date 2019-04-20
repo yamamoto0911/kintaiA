d8c157b6f6158ee2799f4b1ad85f586dbe137a9f
yamamoto yuya <chuo.yamamoto0911@gmail.com>
2019-04-20 06:51:53 +0000

    start btn finish

---
+++ app/controllers/attendances_controller.rb
@@ -0,0 +1,14 @@
+class AttendancesController < ApplicationController
+
+  def create
+    @user = User.find(params[:user_id])
+    @attendance = @user.attendances.find_by(worked_on: Date.today)
+    if @attendance.started_at.nil?
+      @attendance.update_attributes(started_at: current_time)
+      flash[:info] = "おはようございます。"
+    else
+      flash[:danger] = "トラブルがあり、登録出来ませんでした。"
+    end
+    redirect_to @user
+  end
+end
