4c6e9a39d5852a7f62ec65610ecf89795d84f886
yamamoto yuya <chuo.yamamoto0911@gmail.com>
2019-04-20 08:19:48 +0000

    finish attendances show page

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -6,6 +6,9 @@ class AttendancesController < ApplicationController
     if @attendance.started_at.nil?
       @attendance.update_attributes(started_at: current_time)
       flash[:info] = "おはようございます。"
+    elsif @attendance.finished_at.nil?
+      @attendance.update_attributes(finished_at: current_time)
+      flash[:info] = "お疲れ様でした。"    
     else
       flash[:danger] = "トラブルがあり、登録出来ませんでした。"
     end
--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -8,4 +8,13 @@ module AttendancesHelper
       Time.now.min, 0
     )
   end
+  
+  def working_times(started_at, finished_at)
+    format("%.2f", (((finished_at - started_at) /60) / 60.0))      
+  end
+  
+  def working_times_sum(seconds)
+    format("%.2f", seconds / 60 / 60.0)
+  end
+  
 end
