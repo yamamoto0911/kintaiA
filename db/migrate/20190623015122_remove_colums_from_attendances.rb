09118a5ae4fd1fceb7d919bd8a021951190f5f6e
Yamamoto Yuya <champion.yamamoto@gmail.com>
2020-05-10 10:20:53 +0000

    migration destoy

--- db/migrate/20190623015122_remove_colums_from_attendances.rb
+++
@@ -1,6 +0,0 @@
-class RemoveColumsFromAttendances < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :attendances, :superior_name, :string
-    remove_column :attendances, :superior_id, :integer
-  end
-end
